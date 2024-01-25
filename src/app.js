bbcdee2690bbd3b89f421713d029dc8cb7cb925b
nbogie <neillbogie@googlemail.com>
2024-01-25 21:38:49 +0000

    added a db-based route /products

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
 
 //configure the server
 
 app.get("/", (req, res) => {
     res.send("Ok here is the root document response.  try /randomRoll");
+});
+
+app.get("/products", async (req, res) => {
+    const dbResult = await pool.query("select * from products limit 20");
+    const rows = dbResult.rows;
+    console.log("queried db and got : " + dbResult.rowCount + " row(s)");
+    res.json(rows);
 });
 
 app.get("/randomRoll", (req, res) => {
